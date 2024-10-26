8d30a7d32ea91acbb4aff686fca402a680cde2b0
sahaMainak70 <sahamainak70@gmail.com>
2024-10-27 00:40:47 +0530

    Adding Origin

--- crud-backend/config/allowedOrigins.js
+++ crud-backend/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   "http://localhost:3500",
   "http://127.0.0.1:5500",
-  "https://www.google.com",
   "http://localhost:5173",
+  "https://crud-lyart-two.vercel.app",
 ];
 
 module.exports = allowedOrigins;
